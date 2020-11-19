d10a40fcc5023cc91ac5debf89a267a786876433
Doran Kayoumi <dorankayoumi@gmail.com>
2020-11-19 19:19:10 +0100

    Clean up

--- speck_break.py
+++ speck_break.py
@@ -7,15 +7,23 @@ def xor(a,b):
     return strxor.strxor(a,b)
 
 
-def list_duplicates(seq):
+def list_duplicates(haystack):
     """
+    Find all the duplicates in a given list
+    
     source: https://stackoverflow.com/a/5419576
+
+    @type haystack: [bytes]
+    @param haystack: list in which to find duplicates
+
+    @rtype: [(bytes, [int])]
+    @returns: A list of all the duplicates and the index where they were found
     """
     tally = defaultdict(list)
-    for i, item in enumerate(seq):
+    for i, item in enumerate(haystack):
         tally[item].append(i)
 
-    return ((key, locs) for key, locs in tally.items() if len(locs)>1)
+    return ((key, locs) for key, locs in tally.items() if len(locs) > 1)
 
 def main():
     BLOCK_SIZE = 4
@@ -26,23 +34,12 @@ def main():
     blocks = [ct[i:i+BLOCK_SIZE] for i in range(0, len(ct), BLOCK_SIZE)]
 
     dup_blocks = sorted(list_duplicates(blocks))
+
     for dup in dup_blocks:
-        print(dup)
-
-    i1 = dup_blocks[0][1][0] - 1
-    i2 = dup_blocks[0][1][1] - 1
-
-    print(xor(blocks[i1], blocks[i2]))
-
-    i1 = dup_blocks[1][1][0] - 1
-    i2 = dup_blocks[1][1][1] - 1
-
-    print(xor(blocks[i1], blocks[i2]))
-    # pwd|0000|pwd|0000
+        i1 = dup[1][0] - 1
+        i2 = dup[1][1] - 1
+        print(xor(blocks[i1], blocks[i2]))
     
-
-
-
 
 if __name__ == '__main__':
     main()
